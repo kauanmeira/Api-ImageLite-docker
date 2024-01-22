165aca9400cf964b86ee9f0a36fb6232be07ce95
kauan.meira <kauangabriel.meira123@gmail.com>
2024-01-22 16:19:36 -0300

    Inserindo imagem no bd

---
+++ src/main/java/io/github/dougllasfps/imageliteapi/infra/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package io.github.dougllasfps.imageliteapi.infra.repository;
+
+import io.github.dougllasfps.imageliteapi.domain.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository  extends JpaRepository<Image, String> {
+
+
+}
